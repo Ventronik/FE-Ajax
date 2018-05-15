56068a2ee8aae3f597644bac90b65d055c5dce19
Dustin Ruskell <druskell@gmail.com>
2018-05-14 18:47:46 -0700

    fully fuctional ready to deploy

--- js/index.js
+++ js/index.js
@@ -34,21 +34,47 @@
   }
 
   function removePost(post){
-    // FILL IN HERE
     let uuid = post.getAttribute('uuid')
-    
+
     request(`/${uuid}`, 'delete')
     .then(()=> window.location.reload())
   }
 
   function editPost(post){
-    // FILL IN HERE
+    let parent = post.parentNode
+    let title = parent.firstElementChild
+    let body = title.nextElementSibling.innerHTML
+    let titleText = title.innerHTML
     let uuid = post.getAttribute('uuid')
 
+    let newTitle = document.querySelector('#editTitle')
+    newTitle.value = title.innerHTML
+    document.querySelector('#editBody').innerHTML = body
+    newTitle.setAttribute('uuid', uuid)
+  }
+
+// function saveChanges(){
+//     let body = document.querySelector('#editBody').innerHTML
+//     let title = document.querySelector('#editTitle').value
+//     let uuid = document.querySelector('#editTitle').getAttribute('uuid'))
+//       request(`/${uuid}`, 'put', {title, body})
+//       // .then(()=> window.location.reload())
+// }
+
+  function editSubmit (event) {
+    event.preventDefault()
+    // let parent = document.querySelector('#post-edit')
+    let title = document.querySelector('#editTitle').value
+    let body = document.querySelector('#editBody').value
+    let uuid = document.querySelector('#editTitle').getAttribute('uuid')
+    console.log(title, body, uuid)
+    request(`/${uuid}`, 'put', {title, body})
+    .then(()=> window.location.reload())
   }
 
   let form = document.querySelector('#post-form')
   form.addEventListener('submit',(event) => submit(event))
 
-
+  let editForm = document.querySelector('#post-edit')
+  editForm.addEventListener('submit',(event) => editSubmit(event))
 })()
